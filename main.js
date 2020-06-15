6bf60e573e204a89dd86d3b0d4bdb234e509c51b
Ricard Fíguls Mateu <ricard.figuls@altran.com>
2020-06-15 11:03:36 +0200

    ADD status and headers (content-length recalculated)

--- main.js
+++ main.js
@@ -1,13 +1,16 @@
 const http = require('http'), httpProxy = require('http-proxy');
 const rules = require('./rules.json');
 var proxy = httpProxy.createProxyServer();
-
+const OPTIONS = {
+    response: true
+}
 http.createServer(function (req, res) {
 
     var option = {
         target: `http://${req.headers['host']}`,
         selfHandleResponse: true
     };
+
 /*
     proxy.on('proxyReq', function (proxyReq, req, res) {
         res.end('intercepted!')
@@ -31,8 +34,11 @@ http.createServer(function (req, res) {
                 const response = Buffer.concat(body);
                 console.log(`${this.statusCode} ${this.req.method} ${this.req._headers['host']} ${this.req.path} HTTP/${this.httpVersion}`)
                 res.statusCode = this.statusCode;
-                if (rules[this.req.path]) {
-                    res.end(JSON.stringify(rules[this.req.path]))
+                if (OPTIONS.response && rules.response[this.req.path]) {
+                    res.statusCode = rules.response[this.req.path].status || res.statusCode
+                    switchHeaders(res, rules.response[this.req.path].headers)
+                    switchHeaders(res, {"content-length": JSON.stringify(rules.response[this.req.path].body).length})
+                    res.end(JSON.stringify(rules.response[this.req.path].body))
                 } else {
                     res.end(response);
                 }
