44d832f7a516fa55ebe0ab0f77f5092e52e492d7
Ricard Fíguls Mateu <ricard.figuls@gmail.com>
2020-07-02 20:10:03 +0200

    ADD zip for mac

---
+++ zip.js
@@ -0,0 +1,26 @@
+var options = {
+    all: false,
+    dir: './',
+    'app-copyright': 'Ricard Fíguls',
+    'app-version': version,
+    icon: './assets/icon.png',
+    name: 'HTTPInterceptor',
+    ignore: ['.build/', './.git', '/.nyc_output', '/coverage', '.auth.json', '.build.js', '/tests', '.myteam', '.pdf', '.docx'],
+    out: './build',
+    overwrite: true,
+    prune: true,
+    version: version,
+    'version-string': {
+      CompanyName: 'RFM Software',
+      FileDescription: 'HTTPInterceptor',
+      OriginalFilename: 'HTTPInterceptor',
+      ProductName: 'HTTPInterceptor',
+      InternalName: 'HTTPInterceptor'
+    }
+  }
+const paths = await packager(options)
+paths.forEach(async p => {
+    const name = p.split(' /').pop()
+    await zip('./' + p, './' + name + '.zip')
+    console.log(p + '.zip file created!')
+  })
