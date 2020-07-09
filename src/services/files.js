f9197f88f335711b499e81e10f9d235b21cdcd21
Ricard Fíguls Mateu <ricard.figuls@gmail.com>
2020-07-09 18:33:10 +0200

    Fix constant name

--- src/services/files.js
+++ src/services/files.js
@@ -1,11 +1,11 @@
 const lj = require('load-json-file')
 const rra = require('recursive-readdir-async')
 const path = require('path')
-const name = '.http_interceptor'
+const FOLDER_NAME = '.http_interceptor'
 
 const files = {
   homeDir: () => require('os').homedir(),
-  appDir: () => files.homeDir() + path.sep + name + path.sep,
+  appDir: () => files.homeDir() + path.sep + FOLDER_NAME + path.sep,
   sep: () => path.sep,
   getFiles: (path) => rra.list(path),
   loadJson: (file) => lj(file)
